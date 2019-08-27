5bb8c7462bdf6d47203879f200923df6bc50a377
Max Eshleman <meshleman@pivotal.io>
2019-08-27 11:13:30 -0600

    clean up agent test
    Signed-off-by: Caitlyn Yu <cyu@pivotal.io>

--- src/app/agent/agent_test.go
+++ src/app/agent/agent_test.go
@@ -2,6 +2,7 @@ package agent_test
 
 import (
 	"fmt"
+	"github.com/onsi/gomega/types"
 	"log"
 	"net/http"
 
@@ -11,96 +12,68 @@ import (
 	"code.cloudfoundry.org/leadership-election/app/agent"
 )
 
-var run int = 10000
+var run = 10000
 
 var _ = Describe("Agent", func() {
-	var (
-		a map[string]*agent.Agent
-	)
+	var agents map[string]*agent.Agent
 
 	BeforeEach(func() {
-		a = make(map[string]*agent.Agent)
+		agents = make(map[string]*agent.Agent)
+
+		var nodes []string
+
+		// There are 3 intra network addresses and 7 fake addresses to simulate unresponsive agents
+		for i := 3; i <= 12; i++ {
+			nodes = append(nodes, fmt.Sprintf("127.0.0.1:%d", run+i))
+		}
 
 		for i := 0; i < 3; i++ {
-			g := agent.New(
+			a := agent.New(
 				i,
-				[]string{
-					// Intra network addresses
-					fmt.Sprintf("127.0.0.1:%d", run+3),
-					fmt.Sprintf("127.0.0.1:%d", run+4),
-					fmt.Sprintf("127.0.0.1:%d", run+5),
-
-					// Fake and not serviced
-					fmt.Sprintf("127.0.0.1:%d", run+6),
-					fmt.Sprintf("127.0.0.1:%d", run+7),
-					fmt.Sprintf("127.0.0.1:%d", run+8),
-					fmt.Sprintf("127.0.0.1:%d", run+9),
-					fmt.Sprintf("127.0.0.1:%d", run+10),
-					fmt.Sprintf("127.0.0.1:%d", run+11),
-					fmt.Sprintf("127.0.0.1:%d", run+12),
-				},
+				nodes,
 
 				// External address
 				agent.WithPort(run+i),
 				agent.WithLogger(log.New(GinkgoWriter, fmt.Sprintf("[AGENT %d]", i), log.LstdFlags)),
 			)
-			g.Start()
-			u := fmt.Sprintf("http://%s/v1/leader", g.Addr())
-
-			a[u] = g
+			a.Start()
+			agents[fmt.Sprintf("http://%s/v1/leader", a.Addr())] = a
 		}
 	})
 
 	AfterEach(func() {
-		run += 2 * len(a)
+		run += 2 * len(agents)
 
 		// We set up fake and not serviced addresses
 		run += 7
 	})
 
 	It("returns a 200 if it is the leader", func() {
-		var c []chan int
-
-		for addr := range a {
-			cc := make(chan int)
-			c = append(c, cc)
-			go func(addr string, cc chan int) {
-				defer GinkgoRecover()
-				for {
-					resp, err := http.Get(addr)
-					Expect(err).ToNot(HaveOccurred())
-
-					if resp.StatusCode == http.StatusOK {
-						cc <- 1
-					}
-
-					if resp.StatusCode == http.StatusLocked {
-						cc <- 0
-					}
-
-					Expect(resp.StatusCode).To(Or(Equal(http.StatusOK), Equal(http.StatusLocked)))
-				}
-			}(addr, cc)
-		}
-
-		Eventually(func() int {
-			var l0, l1, l2 int
-
-			Eventually(c[0]).Should(Receive(&l0))
-			Eventually(c[1]).Should(Receive(&l1))
-			Eventually(c[2]).Should(Receive(&l2))
-
-			return l0 + l1 + l2
-		}, 10).Should(Equal(1))
-
-		Consistently(func() int {
-			var l0, l1, l2 int
-
-			Eventually(c[0]).Should(Receive(&l0))
-			Eventually(c[1]).Should(Receive(&l1))
-			Eventually(c[2]).Should(Receive(&l2))
-
-			return l0 + l1 + l2
-		}, 3).Should(Equal(1))
+		Eventually(getLeaderStatusFunc(agents), 10).Should(haveSingleLeader(agents))
+		Consistently(getLeaderStatusFunc(agents), 3).Should(haveSingleLeader(agents))
 	})
 })
+
+func getLeaderStatusFunc(agents map[string]*agent.Agent) func() []int {
+	return func() []int {
+		var responses []int
+		for addr := range agents {
+			resp, err := http.Get(addr)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(resp.StatusCode).To(Or(Equal(http.StatusOK), Equal(http.StatusLocked)))
+
+			responses = append(responses, resp.StatusCode)
+		}
+
+		return responses
+	}
+}
+
+func haveSingleLeader(agents map[string]*agent.Agent) types.GomegaMatcher {
+	var nonLeaders []int
+	for i := 1; i < len(agents); i++ {
+		nonLeaders = append(nonLeaders, http.StatusLocked)
+	}
+
+	return ConsistOf(append(nonLeaders, http.StatusOK))
+}
